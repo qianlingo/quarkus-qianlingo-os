0671b7e289db0861962ec5ee26a86a98c89031b1
N.G. QianLinGooo <qianling@qianlingo.com>
2022-03-01 16:46:18 +0800

    QianLinGooo
    first commit

---
+++ quarkus-qianlingo-os-web/src/test/java/com/qianlingo/quarkusos/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.qianlingo.quarkusos;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
